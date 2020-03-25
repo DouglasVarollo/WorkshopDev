6cbbfb2b0fc7aa98e97e6b18eb22678161ec3ebd
Douglas Varollo <dcvarollo@gmail.com>
2020-03-25 16:51:58 -0300

    página de ideias e revisões no código

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
 function onOff() {
+  document.querySelector("body").classList.toggle("hideScroll");
+  document.querySelector("#modal").classList.toggle("addScroll");
   document.querySelector("#modal").classList.toggle("hide");
 }
